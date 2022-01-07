6d114d5bdeed7ff5c43c2dcd17f3c1a612bc8e54
Matryoshka6371 <75605938+Matryoshka6371@users.noreply.github.com>
2022-01-07 22:43:22 +0800

    tailwindcss生效,开始写项目

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-  return <div className="text-blue-800">ybb</div>;
+  return <div className="text-blue-800 text-9xl">ybb</div>;
 }
 
 export default App;
