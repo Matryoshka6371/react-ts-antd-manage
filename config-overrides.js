eec50e03f693e7ae3efb6d2a4a852d6af4cac516
Matryoshka6371 <75605938+Matryoshka6371@users.noreply.github.com>
2022-01-07 22:30:34 +0800

    no message

---
+++ config-overrides.js
@@ -0,0 +1,20 @@
+const { override, addDecoratorsLegacy, addLessLoader } = require('customize-cra');
+
+module.exports = override(
+  // less
+  // addLessLoader(),
+  addLessLoader({
+    lessOptions: {
+      javascriptEnabled: true,
+      // Optionally adjust URLs to be relative. When false, URLs are already relative to the entry less file.
+      relativeUrls: false,
+      modifyVars: { '@primary-color': '#A80000' }
+      // cssModules: {
+      //   // if you use CSS Modules, and custom `localIdentName`, default is '[local]--[hash:base64:5]'.
+      //   localIdentName: "[path][name]__[local]--[hash:base64:5]",
+      // }
+    }
+  }),
+  // enable legacy decorators babel plugin
+  addDecoratorsLegacy()
+);
