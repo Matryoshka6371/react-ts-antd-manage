4d1eff1307fe41635c0b4e9787eedc49d89e519f
Matryoshka6371 <75605938+Matryoshka6371@users.noreply.github.com>
2022-01-08 16:44:42 +0800

    no message

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { Button } from 'antd';
 
-function App() {
-  return <div className="text-blue-800 text-9xl">ybb</div>;
-}
+const App: FC = () => {
+  return (
+    <div className="text-blue-800 text-2xl">
+      <span> 我是按钮外面的文字</span>
+      <Button type="primary">Button</Button>
+    </div>
+  );
+};
 
 export default App;
